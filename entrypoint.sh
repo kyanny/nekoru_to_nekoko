b9297983d58280941462cccdb8ff6ee35ba6a491
Kensuke Nagae <kyanny@gmail.com>
2020-05-09 11:09:22 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eu
 
+env
+
 instagram-scraper --latest-stamps timestamp.txt $INSTAGRAM_USERNAME
 
 ls $INSTAGRAM_USERNAME/*_n.jpg | xargs -I {} curl -X POST -H "Authorization: Bearer ${LINE_NOTIFY_ACCESS_TOKEN}" -F "message=写真が投稿されました" -F "imageFile=@{}" https://notify-api.line.me/api/notify
